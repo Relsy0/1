e1fd920d5460fe6459aff2d168242357c72ab22d
Relsy0 <2472926467@qq.com>
2025-04-06 21:30:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-pip install Flask Flask-Uploads
-
-from flask import Flask, send_from_directory, render_template
-import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
-
-# 配置上传文件夹
-UPLOAD_FOLDER = 'uploads/music'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-@app.route('/music/random_music.mid')
-def uploaded_file():
-    return send_from_directory(app.config['UPLOAD_FOLDER'], 'random_music.mid')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/music/<filename>')
+def music(filename):
+    return send_from_directory('.', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
