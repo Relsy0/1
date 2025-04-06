eb7e038c946286872b62cccd36718db4c71d6076
Relsy0 <2472926467@qq.com>
2025-04-06 21:16:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template
 import os
 
 app = Flask(__name__)
@@ -10,25 +10,9 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'music' not in request.files:
-        return jsonify(success=False, error='没有文件上传')
-    
-    file = request.files['music']
-    if file.filename == '':
-        return jsonify(success=False, error='没有选择文件')
-    
-    # 将文件名强制改为 random_music.mid
-    filename = os.path.join(app.config['UPLOAD_FOLDER'], 'random_music.mid')
-    file.save(filename)
-    
-    url = request.url_root + 'music/' + os.path.basename(filename)
-    return jsonify(success=True, url=url)
-
-@app.route('/music/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+@app.route('/music/random_music.mid')
+def uploaded_file():
+    return send_from_directory(app.config['UPLOAD_FOLDER'], 'random_music.mid')
 
 @app.route('/')
 def index():
